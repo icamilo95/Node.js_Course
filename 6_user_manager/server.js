c45aa127a3ae3d0d7a23d9f6e9b0118c3d652bd1
Camilo Ordonez <icoj95@gmail.com>
2015-05-31 13:52:09 -0700

    Update from CRUD working

--- 6_user_manager/server.js
+++ 6_user_manager/server.js
@@ -9,12 +9,19 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ //This is the middleware, It sets the body parser to work in the middleware
     extended:true
 }));
+//Set up method override --> It helps the form to set the put and delete 
+var methodOverride = require("method-override");
+app.use(methodOverride("_method"));
 
 app.set("view engine", "ejs");
 
 
-app.get('/all', function(req,res){
-   res.render("all");
+app.get('/', function(req,res){
+   models.User.findAll().done(function(users, error){
+      res.render("index", {
+         allUsers: users
+      });   
+   });   
 });
 
 app.get('/new', function(req,res){
@@ -22,7 +29,24 @@ app.get('/new', function(req,res){
 });
 
 app.get('/edit/:id', function(req,res){
-   res.render("edit");
+   models.User.findById(req.params.id).done(function(user) {
+      res.render("edit", {
+         userInfo: user
+      });
+   });
+});
+
+app.put('/edit/:id', function(req,res){
+   models.User.findById(req.params.id).done(function(user) {
+      user.updateAttributes({
+        firstname:req.body.firstname,
+        lastname:req.body.lastname,    
+        username:req.body.username,
+        age:req.body.age,
+      }).done(function(){
+      res.redirect("/");
+   });
+   });
 });
 
 app.post('/new', function(req,res){
@@ -32,7 +56,7 @@ app.post('/new', function(req,res){
       age: req.body.age,
       username: req.body.username
    }).done(function(){ // I put this callback here cause JS is asynchronous so it'll do this inmediately after getting the information
-      res.redirect("/all");
+      res.redirect("/");
    });
 });
 
